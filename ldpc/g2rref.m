b87c4e8a75c00433f9db39e323c6dca62caca79b
kimheeseo <43293606+kimheeseo@users.noreply.github.com>
2022-07-12 01:37:33 +0900

    Add files via upload

---
+++ ldpc/g2rref.m
@@ -0,0 +1,44 @@
+clear all; clc;
+%A2=[1 0 1 1 1;0 0 0 1 1;0 1 0 1 1;0 1 0 0 1];
+%A2=[1 0 1 0 1;0 0 0 1 1;0 1 0 1 0;0 1 0 0 1];
+%A2=[1 0 1 1 1;0 1 0 1 1;0 1 0 0 1;0 0 0 1 0];
+A2=[0 1 0 1 1; 0 0 1 1 1;1 0 1 1 1;1 0 0 0 1];
+A=A2;
+[m,n] = size(A);
+
+% Loop over the entire matrix.
+i = 1;
+j = 1;
+
+while (i <= 3) && (j <= n)
+   % Find value and index of largest element in the remainder of column j.
+   k = find(A(i:m,j),m) + i - 1;
+   k2=min(k);
+   % Swap i-th and k-th rows.
+   A([i k2],j:n) = A([k2 i],j:n);
+   
+   k = find(A(i:m,j),m) + i - 1;
+   k2=min(k);
+   if length(k) ~=1
+    for h=2:length(k)
+        A(k(h),:)=A(k2,:)+A(k(h),:);
+        A(k(h),:)=mod(A(k(h),:),2);
+    end
+   end
+
+   if isempty(k)
+    j=j+1;
+    k = find(A(i:m,j),m) + i - 1;
+    k2=min(k);
+    if length(k) ~=1
+    for h=2:length(k)
+        A(k(h),:)=A(k2,:)+A(k(h),:);
+        A(k(h),:)=mod(A(k(h),:),2);
+    end
+   end
+   end
+
+   i = i + 1;
+   j = j + 1;
+end
+A
